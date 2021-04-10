750211056a4f74749aa5542ebf93bc78a2b7a6ac
chinmayingle <chinmay.r.ingle@gmail.com>
2021-04-11 00:57:47 +0530

    added comments to exceptiontest code

--- exceptiontest.py
+++ exceptiontest.py
@@ -1,22 +1,20 @@
 try:
-    10/0
-    f=open("cool2","r")
-    print("HI")
+    f=open("cool2","w")
+    # print("HI")
     f.write("lol!")
-    print("not printed")
+    # print("not printed if encounted error")
 
 except IOError:
-    print("boolloo na ")
+    print("file io error ")
 except TypeError:
-    print("hmmm")
-except :
-    print("all")
-
+    print("different type than excepted")
+#catches all the exceptions
+except Exception as e:
+    print("Exception: ", e)
 
 # Executed when no exception
 else:
     print("all izzz well!")
-
 
 finally:
     print("finally!!")
